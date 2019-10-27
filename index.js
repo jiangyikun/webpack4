f2aaf2ff2237a611915ad25af5d318ef8423ff0e
jiangyikun <1430988321@qq.com>
2019-10-27 20:42:31 +0800

    1027-first-commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//ES module 模块引入方式
+import Header from './header';
+import SideBar from './sideBar';
+import Content from './content';
+
+new Header();
+new SideBar();
+new Content(); 
---
+++ sideBar.js
@@ -0,0 +1,7 @@
+function SideBar() {
+  var dom = document.getElementById('root');
+  var SideBar = document.createElement("div");
+  SideBar.innerText = "SideBar";
+  dom.append(SideBar);
+}
+export default SideBar
