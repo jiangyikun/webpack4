8d25942570ac0e86192eb06378baae15c46cad59
jiangyikun <1430988321@qq.com>
2019-11-02 00:30:20 +0800

    1101-样式资源打包

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 //ES module 模块引入方式
-import Header from './header';
-import SideBar from './sideBar';
-import Content from './content';
+// import Header from './header';
+// import SideBar from './sideBar';
+// import Content from './content';
+var image1 = require('./static/images/image1.jpg');
+import './index.scss'
 
-new Header();
-new SideBar();
-new Content(); 
+console.log(image1);
+
+var root = document.getElementById('root');
+var img = new Image();
+img.src = image1;
+img.classList.add('img');
+root.append(img);
+
+// new Header();
+// new SideBar();
+// new Content(); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,38 @@
 const path = require("path");
 
 module.exports = {
-  mode:'development',//production生产环境
-  entry: './src/index.js',
+  mode: "development", //production生产环境
+  entry: "./src/index.js",
   output: {
     filename: "min.js",
     path: path.resolve(__dirname, "dist")
   },
-  module:{
-    rules:[{
-      test:/\.jpg$/,
-      use:{
-        loader:'file-loader'
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: {
+          loader: "url-loader", //url-loader可以打包成base64的图片，仅推荐占用大小不大的图片使用，要不然加载js文件会很长，有点可以网页图片的http请求
+          options: {
+            name: "[name]_[hash].[ext]", // 打包出的图片名字规范
+            outputPath: "images/", //打包出文件的文件夹
+            limit: 2048 //表示图片超过2948字节(2kb)就会单独打包出来不会打包成base64放到js文件中
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", 
+          "css-loader", 
+          "sass-loader", 
+          "postcss-loader",//自动在css3样式中添加浏览器前缀
+        ] //loader执行顺序，从下到上，从右到左
       }
-    }]
+    ]
   }
 };
